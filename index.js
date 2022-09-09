4ff51275661d02ac1d5e841d6c483c9cffb82dc7
Steven McVey <smcvey1210@gmail.com>
2022-09-08 21:52:38 -0700

    add handleCreateUser function

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const users = {};
+let hasAccount = false;
+let isLoggedIn = false;
+let loggedInUser
 
 fetch(`http://localhost:3000/users`, {
         method: "GET",
@@ -6,14 +9,35 @@ fetch(`http://localhost:3000/users`, {
           "Content-Type": "application/json",
           "Accept": "application/json"
         }
+})
+.then(res => res.json())
+.then(data => {
+  for (const user of data){
+    users[user.username] = {
+      password: user.password,
+      points: user.points,
+      id: user.id,
+    }
+  }
+})
+
+function handleCreateUser(username, password){
+  if (users[`${username}`] === undefined){
+    fetch(`http://localhost:3000/users`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({
+          "username": username,
+          "points": 0,
+          "password": password,
+        })
       })
-      .then(res => res.json())
-      .then(data => {
-        for (const user of data){
-          users[user.username] = {
-            password: user.password,
-            points: user.points,
-            id: user.id,
-          }
-        }
-      })
+    .then(() => alert(`Account successfully created`))
+    .catch(() => alert(`An error occurred. Please try again.`))
+  }else{
+    alert('Username already exists. Please try another name or log in if you already have an account')
+  }
+}
